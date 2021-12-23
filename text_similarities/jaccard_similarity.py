d57ff2d2379d34e8e4c8dd9bd6e4348da6850c1c
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2021-12-23 16:01:34 -0500

    Delete text_similarities directory

--- text_similarities/jaccard_similarity.py
+++
@@ -1,9 +0,0 @@
-def jaccard_similarity(textA, textB):
-  textA = set(textA.split())
-  textB = set(textB.split())
-  intersection = set(textA).intersection(set(textB))
-  union = set(textA).union(textB)
-  similarity = len(intersection)/len(union)
-  return similarity
-
-jaccard_similarity(textA, textB)
